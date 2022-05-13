b78ea80a3dc8e276bb729d48601bf1402b3c5714
karagoz-ziya <sar581999@gmail.com>
2022-05-13 12:14:58 +0300

    Update RecycleMap.jsx

--- src/pages/RecycleMap.jsx
+++ src/pages/RecycleMap.jsx
@@ -6,7 +6,7 @@ const RecycleMap = () => {
 
   return (
     <div className='flex h-screen w-full bg-gradient-to-r from-purple-500 to-blue-200 justify-center items-center text-blue-600'>
-      <div className='col-span-6 mt-5 bg-opacity-50 border border-gray-500 rounded shadow-sm  hover:shadow-lg duration-500  md:col-span-3 lg:col-span-2  '>
+      <div className='col-span-6 mt-5 bg-opacity-50 border border-gray-300 rounded shadow-sm  hover:shadow-lg duration-500  md:col-span-3 lg:col-span-2  '>
         <div className='flex flex-row px-2 py-3 mx-3 justify-center'>
           <div className='flex flex-col mt-1 mb-2 ml-4'>
             <div className='text-2xl  font-bold'>Recycle Mapp</div>
@@ -22,19 +22,21 @@ const RecycleMap = () => {
 
         <div className='mb-5 border-t mx-3 '>
           <div className='flex flex-wrap  mx-5 mt-6 text-xs justify-center '>
-            <div className='flex mb-2 mr-4 font-normal  underline text-lg'>
-              <button className='border'>
+            <button className='border rounded-md bg-gradient-to-tr from-purple-200 hover:shadow-xl'>
+              <div className='flex my-1 mx-3 font-bold text-lg justify-center'>
                 <a href='app-debug.apk' download>
                   Download
                 </a>
-              </button>
-            </div>
-            <div className='flex w-full mt-1 justify-end '>
-              <div className='mr-1 text-xs text-fuchsia-600 cursor-pointer hover:italic font-base'>
-                By Ziya Karagöz
               </div>
-            </div>
+            </button>
+
+            <div className='flex w-full mt-1 justify-end '></div>
           </div>
+        </div>
+        <div className='flex cursor-pointer text-right justify-end font-base'>
+          <p className=' hover:italic mx-1 my-1 text-fuchsia-600 text-xs'>
+            By Ziya Karagöz
+          </p>
         </div>
       </div>
     </div>
